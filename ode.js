d5e08386989e9c5b9042e05a8cdd2fe7771620c5
Xavier Via <fernando.via@gmail.com>
2014-11-22 23:54:36 +0100

    Add more informative message on no arguments

--- ode.js
+++ ode.js
@@ -53,12 +53,30 @@ var example   = require('washington')
 var assert    = require('assert')
 var metadata  = require('./package')
 
+var BOLD      = "\u001b[1m"
+var CLEAR     = "\u001b[0m"
+
 require('coffee-script/register')
 
 var Ode = function (argv, callback, req, cwd) {
   var libraryName, library, options = {}
 
   argv = argv || process.argv
+
+  argv = argv.slice(2)
+
+  if (argv.length === 0)
+    return console.log(
+      BOLD + "Ode (v" + metadata.version + "):" + CLEAR +
+      " the task runner that adds no dependencies to your code.\n\n" +
+      "Usage:\n\n" +
+      "  ode <yourfile>[.js|.coffee] --<someFlag>=<someValue> \\\n" +
+      "                              --<someOtherFlag>\n\n" +
+      "The flags will be casted as properties in the options object sent to\n" +
+      "your file's main function.\n\n" +
+      "To see the version number, run:\n\n" +
+      "  ode -v\n\n" +
+      "or just run ode without arguments and you will see this message again.")
 
   if (argv.indexOf("-v") !== -1)
     return console.log(metadata.version)
@@ -106,7 +124,7 @@ example('Casts array of args into options object and sends it to library', funct
     return library }
 
   //! WHEN
-  Ode(["--string=flag", "library", "--boolean", "--number=3"], null, require, "cwd");
+  Ode([1, 2, "--string=flag", "library", "--boolean", "--number=3"], null, require, "cwd");
 
   //! THEN
   assert.equal(require.calls[0][0], "cwd/library")
@@ -118,7 +136,7 @@ example('Casts array of args into options object and sends it to library', funct
 
 example('Is able to require a .coffee package', function() {
   //! WHEN
-  var value = Ode(["--name=John", "example/hello.coffee"])
+  var value = Ode([1, 2, "--name=John", "example/hello.coffee"])
 
   //! THEN
   assert(value)
@@ -129,7 +147,7 @@ example('Runs the callback when provided', function () {
   var callback = function (options) { callback.options = options }
 
   //! WHEN
-  Ode(["--justName=something"], callback)
+  Ode([1, 2, "--justName=something"], callback)
 
   //! THEN
   assert.equal(callback.options.justName, "something")
@@ -141,7 +159,7 @@ example('Returns the result value from the callback when provided', function () 
     return options.name + " " + options.lastName }
 
   //! WHEN
-  var result = Ode(["--name=Mister", "--lastName=X"], callback)
+  var result = Ode([1, 2, "--name=Mister", "--lastName=X"], callback)
 
   //! THEN
   assert.equal(result, "Mister X")
@@ -153,10 +171,34 @@ example('Shows only the version number when flag -v is sent', function () {
   console.log = function () { console.log.args = arguments }
 
   //! WHEN
-  Ode(["-v"])
+  Ode(["1", "2", "-v"])
 
   //! THEN
   assert.equal(console.log.args[0], metadata.version)
+  console.log = temp.log
+})
+
+example('Shows a helpful message when ran with no arguments', function () {
+  //! GIVEN
+  var temp = { log: console.log }
+  console.log = function () { console.log.args = arguments }
+
+  //! WHEN
+  Ode(["node", "else"])
+
+  //! THEN
+  assert.equal(
+    console.log.args[0],
+    BOLD + "Ode (v" + metadata.version + "):" + CLEAR +
+    " the task runner that adds no dependencies to your code.\n\n" +
+    "Usage:\n\n" +
+    "  ode <yourfile>[.js|.coffee] --<someFlag>=<someValue> \\\n" +
+    "                              --<someOtherFlag>\n\n" +
+    "The flags will be casted as properties in the options object sent to\n" +
+    "your file's main function.\n\n" +
+    "To see the version number, run:\n\n" +
+    "  ode -v\n\n" +
+    "or just run ode without arguments and you will see this message again.")
   console.log = temp.log
 })
 
